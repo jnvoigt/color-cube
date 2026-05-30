ea21a7e14af6f5ad331b18cf5a9d9bd74608c21a
jnvoigt <voigt.julian@gmx.de>
2026-05-30 18:40:12 +0200

    Add grid generation for cube visualization

--- scripts/ColorCube.cs
+++ scripts/ColorCube.cs
@@ -29,6 +29,7 @@ public partial class ColorCube : Node3D
             Colors.Lime,
             Colors.Teal,
             Colors.MediumSpringGreen,
+            Colors.PaleGreen,
             Colors.Cyan,
             Colors.DeepSkyBlue,
             Colors.Blue,
@@ -48,6 +49,50 @@ public partial class ColorCube : Node3D
         {
             CreateBucketFromColor(colorBucket);
         }
+
+        GenerateCubeGrid(3);
+    }
+
+    private void GenerateCubeGrid(int lineCount)
+    {
+        var gridMesh = GetNode<MeshInstance3D>("%GridMesh");
+        var mesh = new ImmediateMesh();
+        gridMesh.Mesh = mesh;
+
+        // generate grid into mesh, primitive lines
+
+        GenerateLines(mesh, lineCount, Vector3.Back, Vector3.Right, Vector3.Up);
+        GenerateLines(mesh, lineCount, Vector3.Right, Vector3.Up, Vector3.Back);
+        GenerateLines(mesh, lineCount, Vector3.Up, Vector3.Back, Vector3.Right);
+    }
+
+    private void GenerateLines(ImmediateMesh mesh, int lineCount, Vector3 direction, Vector3 offset1, Vector3 offset2)
+    {
+        var initialPoint = -(direction + offset1 + offset2) * _bucketFactor / 2;
+        var segmentLength = _bucketFactor / lineCount;
+        
+        for (var offset1Index = 0; offset1Index < lineCount + 1; offset1Index++)
+        {
+            for (var offset2Index = 0; offset2Index < lineCount + 1; offset2Index++)
+            {
+                for (var segment = 0; segment < lineCount; segment++)
+                {
+                    var directionShift = segment*segmentLength*direction;
+                    var offset1Shift = offset1*segmentLength*offset1Index;
+                    var offset2Shift = offset2*segmentLength*offset2Index;
+                    
+                    var start = initialPoint + directionShift + offset1Shift + offset2Shift;
+                    var end = start + segmentLength*direction;
+
+                    mesh.SurfaceBegin(Mesh.PrimitiveType.Lines);
+                    mesh.SurfaceAddVertex(start);
+                    mesh.SurfaceAddVertex(end);
+                    mesh.SurfaceEnd();
+                }
+            }
+            
+        }
+
     }
 
     public void CreateBucketFromColor(Color color)
@@ -63,13 +108,12 @@ public partial class ColorCube : Node3D
         bucketInstance.Factor = _bucketFactor;
         bucketInstance.SetColorBucket(bucket);
         bucketInstance.ShowAccumulatedColor(_showAccumulatedColor);
-        
     }
 
     public void FillBucketsWithImageData(Image image)
     {
         ResetBuckets();
-        
+
         var width = image.GetWidth();
         var height = image.GetHeight();
 
@@ -86,8 +130,12 @@ public partial class ColorCube : Node3D
                 }
             }
         }
-        
+
         Debug.WriteLine($"Add {counter} colors to buckets");
+        foreach (var bucketObject in GetBucketObjects())
+        {
+            bucketObject.UpdateVisuals();
+        }
     }
 
     private void ResetBuckets()
@@ -104,14 +152,16 @@ public partial class ColorCube : Node3D
         var closestBucket = _buckets.OrderBy(b => b.DistanceSquaredTo(color)).First();
         closestBucket.Add(color);
     }
-    
+
     public void ToggleBucketDisplayMode(bool enabled)
     {
         _showAccumulatedColor = enabled;
-        var bucketObjects = GetChildren().OfType<BucketObject>();
+        var bucketObjects = GetBucketObjects();
         foreach (var bucket in bucketObjects)
         {
             bucket.ShowAccumulatedColor(enabled);
         }
     }
+
+    private IEnumerable<BucketObject> GetBucketObjects() => GetChildren().OfType<BucketObject>();
 }
